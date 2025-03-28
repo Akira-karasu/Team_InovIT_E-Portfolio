275a8070b63b3f76d494d94e859bec2459a9e09b
Akira-karasu <irisalacapa010@gmail.com>
2025-03-28 11:39:38 +0800

    E-portfolio updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,13 @@
+import useIntersectionObserver from "../hooks/useIntersectionObserver.jsx";
+
 function AboutPage() {
+    useIntersectionObserver();
     return (
       <div className="con" id="about">
-        <h1 className="poppins-Main-Heading">
+        <h1 className="hiddens poppins-Main-Heading">
           Welcome to the About <span style={{ color: '#FF7D00' }}>Page</span>
         </h1>
-        <p className="poppins-Sub-Heading">This is a simple React component.</p>
+        <p className="hiddens poppins-Sub-Heading">This is a simple React component.</p>
       </div>
     );
   }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,17 @@
-import Button from "../components/button.jsx";
-import IconButton from "../components/iconButton.jsx";
+
+import useIntersectionObserver from "../hooks/useIntersectionObserver.jsx";
 
 function HomePage() {
-    return (
-      <div className="con" id="home">
-        <h1 className="poppins-Main-Heading">
+  useIntersectionObserver();
+
+  return (
+    <div className="con" id="home">
+        <h1 className="hiddens poppins-Main-Heading">
           Welcome to the Home <span style={{ color: '#FF7D00' }}>Page</span>
         </h1>
-        <p className="poppins-Sub-Heading">This is a simple React component.</p>
-        
-        <br />
-        <div className="grid grid-cols-3 gap-4 place-items-center ">
-        <Button text="Front-end" />
-        <Button text="UI/UX" />
-        <Button text="Back-end" />
-        </div>
-        <br />
-        <IconButton />
+        <p className="hiddens poppins-Sub-Heading">This is a simple React component.</p>
       </div>
-    );
-  }
-  
-  export default HomePage;
+  );
+}
+
+export default HomePage;
